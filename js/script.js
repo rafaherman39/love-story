4e68efaca516da890bcf91b18243389e2fb9b98c
rafaherman39 <rafaherman.39@gmail.com>
2026-09-15 21:56:02 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ let autoScrollActive = false;
 let autoScrollFrame = null;
 let lastScrollTime = 0;
 
-const AUTO_SCROLL_SPEED = 6;
+const AUTO_SCROLL_SPEED = 8;
 
 
 /* =====================================================
@@ -244,7 +244,7 @@ function startStory() {
 
         startSlowAutoScroll();
 
-    }, 8000);
+    }, 6000);
 
 }
 
